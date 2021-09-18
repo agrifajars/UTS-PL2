043e6d03d45570c74d94797d3a8d561dbdda30d1
Agrifajars <agri@nawatech.co>
2021-09-18 13:59:12 +0700

    Adding sql file

--- app/config/config.php
+++ app/config/config.php
@@ -5,4 +5,4 @@ define('BASEURL', 'http://localhost/Kuliah/UTS-PL2/public/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'latihan');
+define('DB_NAME', 'db_uts');
